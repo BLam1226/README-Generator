816b5c0e29999627829e593a730758ef92802945
BLam1226 <brendanlam0@gmail.com>
2023-07-29 03:48:13 -0400

    Updated code for all functions and a README

---
+++ README.md
@@ -0,0 +1,37 @@
+
+# README-Generator
+
+## Description 
+  Code to generate a professional README markdown file using the inputted answers received from the user.
+
+## Table of Contents
+* [Project-Title](#project-title)
+* [Project-Description](#project-description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [License](#license)
+* [Questions](#questions)
+
+## Installation
+1. npm install inquirer@8.2.4
+2. npm i
+
+## Usage
+answer the displayed questions using the terminal which will then generate a README file based on the answers received.
+
+## Contributing
+no idea
+
+## Tests
+  npm test
+
+## License
+This project is licensed under the MIT License. Click the License Badge for details.
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Questions
+  GitHub: [BLam1226](https://github.com/BLam1226)
+  Email: brendanlam0@gmail.com
--- index.js
+++ index.js
@@ -1,13 +1,83 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "projectTitle",
+    message: "Enter the title of your project:",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a description of your project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Explain how to install your project:",
+  },
+  {
+    type: "input",
+    name: "dependencies",
+    message: "To install necessary dependencies, run the following command:",
+    default: "npm i",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Explain how to use your project:",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Explain how others can contribute to your project:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Explain how to run tests (if applicable):",
+    default: "npm test",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: ["mit", "apache", "gpl", "isc", "unlicensed", "other"],
+  },
+  {
+    type: "input",
+    name: "githubUsername",
+    message: "Enter your GitHub username:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address:",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`README file "${fileName}" successfully created!`);
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const readmeContent = generateMarkdown(answers);
+    writeToFile("README.md", readmeContent);
+  });
+}
 
 // Function call to initialize app
 init();
